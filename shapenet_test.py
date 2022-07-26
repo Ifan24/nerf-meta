1c323c1668efd74fbf9877b41e64eb802f974bec
Ruiqi <ruiqili1024@gmail.com>
2022-07-26 20:39:13 +1000

    remove mip-nerf code

--- shapenet_test.py
+++ shapenet_test.py
@@ -5,23 +5,21 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset
 from datasets.shapenet import build_shapenet
-from models.nerf import build_nerf, build_MipNerf
+from models.nerf import build_nerf
 from utils.shape_video import create_360_video
-from models.rendering import get_rays_shapenet, sample_points, volume_render, get_d, get_rays_shapenet_mipNerf, get_raybatch
+from models.rendering import get_rays_shapenet, sample_points, volume_render
 from tqdm.notebook import tqdm as tqdm
 import matplotlib.pyplot as plt
-from shapenet_train import report_result, inner_loop, calc_psnr, render_image
-from models.rendering import Rays_keys, Rays
-from livelossplot import PlotLosses
+from shapenet_train import report_result, inner_loop
 
 def test_time_optimize(args, model, optim, imgs, poses, hwf, bound):
     """
     test-time-optimize the meta trained model on available views
     """
-    inner_loop(model, optim, imgs, poses, hwf, bound, args.num_samples, args.tto_batchsize, args.tto_steps, args.model)
+    inner_loop(model, optim, imgs, poses, hwf, bound, args.num_samples, args.tto_batchsize, args.tto_steps)
 
 
-def train_val_scene_simple(args, model, optim, tto_imgs, tto_poses, test_imgs, test_poses, hwf, bound):
+def train_val_scene(args, model, optim, tto_imgs, tto_poses, test_imgs, test_poses, hwf, bound):
     """
     train and val the model on available views
     """
@@ -108,119 +106,7 @@ def train_val_scene_simple(args, model, optim, tto_imgs, tto_poses, test_imgs, t
         elif step > 50000 and step <= 100000:
             train_val_freq = 5000
     print(val_psnrs)
-    
-def train_val_scene(args, model, optim, tto_imgs, tto_poses, test_imgs, test_poses, hwf, bound):
-    """
-    train and val the model on available views
-    """
-    if args.model == 'simple':
-        return train_val_scene_simple(args, model, optim, tto_imgs, tto_poses, test_imgs, test_poses, hwf, bound)
-        
-    coarse_loss_mult = 0.2
-    rays = get_rays_shapenet_mipNerf(hwf, tto_poses)
-    raybatch, val_mask = get_raybatch(rays, tto_imgs, args.tto_batchsize)
-    indices = torch.randint(len(raybatch), size=[args.train_val_steps])
-    
-    test_rays = get_rays_shapenet_mipNerf(hwf, test_poses)
-    train_val_freq = args.train_val_freq
-    
-    
-    if args.tto_views == 1:
-        plt.figure()
-        plt.imshow(tto_imgs[0].cpu())
-        plt.title('Input Image')
-        plt.show()
-        
-    # plt_groups = {'Test PSNR':[]}
-    # plotlosses_model = PlotLosses(groups=plt_groups)
-    # plt_groups['Test PSNR'].append('test')
-    val_psnrs = []
-    for step in tqdm(range(args.train_val_steps), desc = 'Train & Validate'):
-        idx = indices[step]
-        rgbs = raybatch[idx].color
-        rays_batch = raybatch[idx]
-        
-        optim.zero_grad()
-        
-        ret = model(rays_batch, True)
-        # calculate loss for coarse and fine
-        mask = rays_batch.lossmult
-        losses = []
-        for (rgb, _, _) in ret:
-            losses.append((mask * (rgb - rgbs[..., :3]) ** 2).sum() / mask.sum())
-        # The loss is a sum of coarse and fine MSEs
-        mse_corse, mse_fine = losses
-        loss = coarse_loss_mult * mse_corse + mse_fine
-        loss.backward()
-        optim.step()
-        
-        with torch.no_grad():
-            # validation
-            if step % train_val_freq == 0 and step != 0:
-                view_psnrs = []
-                plt.figure(figsize=(15, 6))
-                count = 0
-                for i in range(test_imgs.shape[0]):
-                    img = test_imgs[i]
-                    ray = Rays(
-                        origins=test_rays.origins[i],
-                        directions=test_rays.directions[i],
-                        viewdirs=test_rays.viewdirs[i],
-                        radii=test_rays.radii[i],
-                        lossmult=test_rays.lossmult[i],
-                        near=test_rays.near[i],
-                        far=test_rays.far[i]
-                    )
-                    
-                    rgb_gt = img[..., :3]
-                    coarse_rgb, fine_rgb, val_mask = render_image(model, ray, img, args.test_batchsize)
-                
-                    val_mse_coarse = (val_mask * (coarse_rgb - rgb_gt) ** 2).sum() / val_mask.sum()
-                    val_mse_fine = (val_mask * (fine_rgb - rgb_gt) ** 2).sum() / val_mask.sum()
-                
-                    val_loss = coarse_loss_mult * val_mse_coarse + val_mse_fine
-                    val_psnr_fine = calc_psnr(fine_rgb, rgb_gt)
-                    view_psnrs.append(val_psnr_fine)
-                
-                    fine_rgb = torch.clip(fine_rgb.squeeze(0), min=0, max=1)
-                
-                    if count < args.tto_showImages:
-                        plt.subplot(2, 5, count+1)
-                        plt.imshow(img.cpu())
-                        plt.title('Target')
-                        plt.subplot(2,5,count+6)
-                        plt.imshow(fine_rgb.cpu())
-                        plt.title(f'{val_psnr_fine:0.2f}')
-                    count += 1
-                    
-                plt.show()
-                
-                plt.figure()
-                scene_psnr = torch.stack(view_psnrs).mean().item()
-                val_psnrs.append((step, scene_psnr))
-                print(f"step: {step}, val psnr: {scene_psnr:0.3f}")
-                plt.plot(*zip(*val_psnrs), label="val_psnr")
-                plt.title(f'ShapeNet Reconstruction from {args.tto_views} views')
-                plt.xlabel('Iterations')
-                plt.ylabel('PSNR')
-                plt.legend()
-                plt.show()
 
-                # plotlosses_model.update({'test':scene_psnr}, current_step=step)
-                # plotlosses_model.send()
-            
-            # step           = <=1000 -> <=10000 -> <=50000 -> <=100000
-            # train_val_freq = 100    -> 500     -> 2500    -> 5000
-            if step <= 1000:
-                train_val_freq = 100
-            elif step > 1000 and step <= 10000:
-                train_val_freq = 500
-            elif step > 10000 and step <= 50000:
-                train_val_freq = 2500
-            elif step > 50000 and step <= 100000:
-                train_val_freq = 5000
-                
-    print(val_psnrs)
     
 def test():
     parser = argparse.ArgumentParser(description='shapenet few-shot view synthesis')
@@ -230,8 +116,6 @@ def test():
                         help='path to the meta-trained weight file')
     parser.add_argument('--one_scene', action='store_true', help="train and validate the model on the first scene of test dataset")
     parser.add_argument('--standard_init', action='store_true', help="train and validate the model without meta learning parameters")
-    parser.add_argument('--model', type=str, default='mip', choices=['simple', 'mip', 'ngp'],
-                        help='inner model, (simple, mip, ngp)')
     args = parser.parse_args()
 
     with open(args.config) as config:
@@ -248,11 +132,7 @@ def test():
         test_set = Subset(test_set, range(0, args.max_test_size))
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
 
-    if args.model == 'mip':
-        model = build_MipNerf(args)
-    elif args.model == 'simple':
-        model = build_nerf(args)
-        
+    model = build_nerf(args)
     model.to(device)
 
     if not args.standard_init:
@@ -291,7 +171,7 @@ def test():
 
         test_time_optimize(args, model, optim, tto_imgs, tto_poses, hwf, bound)
         scene_psnr = report_result(model, test_imgs, test_poses, hwf, bound, 
-                                    args.num_samples, args.test_batchsize, args.tto_showImages, args.model)
+                                    args.num_samples, args.test_batchsize, args.tto_showImages)
         
         if args.create_video:
             create_360_video(args, model, hwf, bound, device, idx+1, savedir)
--- shapenet_train.py
+++ shapenet_train.py
@@ -5,178 +5,75 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset
 from datasets.shapenet import build_shapenet
-from models.nerf import build_nerf, build_MipNerf
-from models.rendering import get_rays_shapenet, sample_points, volume_render, get_d, get_rays_shapenet_mipNerf, get_raybatch
-from models.mip import rearrange_render_image
+from models.nerf import build_nerf
+from models.rendering import get_rays_shapenet, sample_points, volume_render
 from tqdm.notebook import tqdm as tqdm
 import matplotlib.pyplot as plt
-from models.rendering import Rays_keys, Rays
 
 
-def calc_mse(x: torch.Tensor, y: torch.Tensor):
-    """
-    Calculates the mean square error between tensors `x` and `y`.
-    """
-    return torch.mean((x - y) ** 2)
-    
-def calc_psnr(x: torch.Tensor, y: torch.Tensor):
-    """
-    Calculates the Peak-signal-to-noise ratio between tensors `x` and `y`.
-    """
-    mse = calc_mse(x, y)
-    psnr = -10.0 * torch.log10(mse)
-    return psnr
-
-def inner_loop(model, optim, imgs, poses, hwf, bound, num_samples, raybatch_size, inner_steps, arch):
+def inner_loop(model, optim, imgs, poses, hwf, bound, num_samples, raybatch_size, inner_steps):
     """
     train the inner model for a specified number of iterations
     """
-    if arch == 'mip':
-        # mult factor for coarse MLP loss
-        coarse_loss_mult = 0.2
-        # imgs = [N, H, W, 3]
+    pixels = imgs.reshape(-1, 3)
+
+    rays_o, rays_d = get_rays_shapenet(hwf, poses)
+    rays_o, rays_d = rays_o.reshape(-1, 3), rays_d.reshape(-1, 3)
+
+    num_rays = rays_d.shape[0]
+    for step in range(inner_steps):
+        indices = torch.randint(num_rays, size=[raybatch_size])
+        raybatch_o, raybatch_d = rays_o[indices], rays_d[indices]
+        pixelbatch = pixels[indices] 
+        t_vals, xyz = sample_points(raybatch_o, raybatch_d, bound[0], bound[1],
+                                    num_samples, perturb=True)
         
-        rays = get_rays_shapenet_mipNerf(hwf, poses)
-        # rays = [N, H, W, 3 or 1]
-        raybatch, val_mask = get_raybatch(rays, imgs, raybatch_size)
-        # len(raybatch) = N*H*W/raybatch_size
-        # raybatch.origins = [raybatch_size, 3]
-        # randomly select N=inner_steps sample from raybatch
-        indices = torch.randint(len(raybatch), size=[inner_steps])
-        
-        for step in range(inner_steps):
-            idx = indices[step]
-            rgbs = raybatch[idx].color
-            rays_batch = raybatch[idx]
-            
-            optim.zero_grad()
-            
-            ret = model(rays_batch, True)
-            # calculate loss for coarse and fine
-            mask = rays_batch.lossmult
-            losses = []
-            for (rgb, _, _) in ret:
-                losses.append((mask * (rgb - rgbs[..., :3]) ** 2).sum() / mask.sum())
-            # The loss is a sum of coarse and fine MSEs
-            mse_corse, mse_fine = losses
-            loss = coarse_loss_mult * mse_corse + mse_fine
-            loss.backward()
-            optim.step()
-            
-    elif arch == 'simple':
-        pixels = imgs.reshape(-1, 3)
-
-        rays_o, rays_d = get_rays_shapenet(hwf, poses)
-        rays_o, rays_d = rays_o.reshape(-1, 3), rays_d.reshape(-1, 3)
+        optim.zero_grad()
+        rgbs, sigmas = model(xyz)
+        colors = volume_render(rgbs, sigmas, t_vals, white_bkgd=True)
+        loss = F.mse_loss(colors, pixelbatch)
+        loss.backward()
+        optim.step()
     
-        num_rays = rays_d.shape[0]
-        for step in range(inner_steps):
-            indices = torch.randint(num_rays, size=[raybatch_size])
-            raybatch_o, raybatch_d = rays_o[indices], rays_d[indices]
-            pixelbatch = pixels[indices] 
-            t_vals, xyz = sample_points(raybatch_o, raybatch_d, bound[0], bound[1],
-                                        num_samples, perturb=True)
-            
-            optim.zero_grad()
-            rgbs, sigmas = model(xyz)
-            colors = volume_render(rgbs, sigmas, t_vals, white_bkgd=True)
-            loss = F.mse_loss(colors, pixelbatch)
-            loss.backward()
-            optim.step()
-    
-            
-
-def render_image(model, rays, rgbs, val_chunk_size):
-    height, width, _ = rgbs.shape  # H W C
-    single_image_rays, val_mask = rearrange_render_image(rays, val_chunk_size)
-    coarse_rgb, fine_rgb = [], []
-    with torch.no_grad():
-        for batch_rays in single_image_rays:
-            (c_rgb, _, _), (f_rgb, _, _) = model(batch_rays, False)
-            coarse_rgb.append(c_rgb)
-            fine_rgb.append(f_rgb)
-
-    coarse_rgb = torch.cat(coarse_rgb, dim=0)
-    fine_rgb = torch.cat(fine_rgb, dim=0)
-
-    coarse_rgb = coarse_rgb.reshape(1, height, width, coarse_rgb.shape[-1])  # N H W C
-    fine_rgb = fine_rgb.reshape(1, height, width, fine_rgb.shape[-1])  # N H W C
-    return coarse_rgb, fine_rgb, val_mask
-        
-    
-def report_result(model, imgs, poses, hwf, bound, num_samples, raybatch_size, tto_showImages, arch):
+def report_result(model, imgs, poses, hwf, bound, num_samples, raybatch_size, tto_showImages):
     """
     report view-synthesis result on heldout views
     """
-    if arch == 'mip':
-        coarse_loss_mult = 0.2
-        rays = get_rays_shapenet_mipNerf(hwf, poses)
+    ray_origins, ray_directions = get_rays_shapenet(hwf, poses)
+    view_psnrs = []
+    fig = plt.figure(figsize=(15, 6))
+    count = 0
+    for img, rays_o, rays_d in zip(imgs, ray_origins, ray_directions):
+        rays_o, rays_d = rays_o.reshape(-1, 3), rays_d.reshape(-1, 3)
+        t_vals, xyz = sample_points(rays_o, rays_d, bound[0], bound[1],
+                                    num_samples, perturb=False)
         
-        view_psnrs = []
-        fig = plt.figure(figsize=(15, 6))
-        count = 0
-        for i in range(imgs.shape[0]):
-            img = imgs[i]
-            ray = Rays(
-                origins=rays.origins[i],
-                directions=rays.directions[i],
-                viewdirs=rays.viewdirs[i],
-                radii=rays.radii[i],
-                lossmult=rays.lossmult[i],
-                near=rays.near[i],
-                far=rays.far[i]
-            )
-            
-            rgb_gt = img[..., :3]
-            coarse_rgb, fine_rgb, val_mask = render_image(model, ray, img, raybatch_size)
-        
-            val_mse_coarse = (val_mask * (coarse_rgb - rgb_gt) ** 2).sum() / val_mask.sum()
-            val_mse_fine = (val_mask * (fine_rgb - rgb_gt) ** 2).sum() / val_mask.sum()
-        
-            val_loss = coarse_loss_mult * val_mse_coarse + val_mse_fine
-            val_psnr_fine = calc_psnr(fine_rgb, rgb_gt)
-            view_psnrs.append(val_psnr_fine)
-        
-            fine_rgb = torch.clip(fine_rgb.squeeze(0), min=0, max=1)
+        synth = []
+        num_rays = rays_d.shape[0]
+        with torch.no_grad():
+            for i in range(0, num_rays, raybatch_size):
+                rgbs_batch, sigmas_batch = model(xyz[i:i+raybatch_size])
+                color_batch = volume_render(rgbs_batch, sigmas_batch, 
+                                            t_vals[i:i+raybatch_size],
+                                            white_bkgd=True)
+                synth.append(color_batch)
+            synth = torch.cat(synth, dim=0).reshape_as(img)
+            error = F.mse_loss(img, synth)
+            psnr = -10*torch.log10(error)
+            view_psnrs.append(psnr)
             
             if count < tto_showImages:
                 plt.subplot(2, 5, count+1)
                 plt.imshow(img.cpu())
                 plt.title('Target')
                 plt.subplot(2,5,count+6)
-                plt.imshow(fine_rgb.cpu())
-                plt.title(f'synth psnr:{val_psnr_fine:0.2f}')
+                plt.imshow(synth.cpu())
+                plt.title(f'synth psnr:{scene_psnr:0.2f}')
             count += 1
             
-        plt.show()
-        
-        scene_psnr = torch.stack(view_psnrs).mean()
-        return scene_psnr
-    
-    elif arch == 'simple':
-        ray_origins, ray_directions = get_rays_shapenet(hwf, poses)
-        view_psnrs = []
-        for img, rays_o, rays_d in zip(imgs, ray_origins, ray_directions):
-            rays_o, rays_d = rays_o.reshape(-1, 3), rays_d.reshape(-1, 3)
-            t_vals, xyz = sample_points(rays_o, rays_d, bound[0], bound[1],
-                                        num_samples, perturb=False)
-            
-            synth = []
-            num_rays = rays_d.shape[0]
-            with torch.no_grad():
-                for i in range(0, num_rays, raybatch_size):
-                    rgbs_batch, sigmas_batch = model(xyz[i:i+raybatch_size])
-                    color_batch = volume_render(rgbs_batch, sigmas_batch, 
-                                                t_vals[i:i+raybatch_size],
-                                                white_bkgd=True)
-                    synth.append(color_batch)
-                synth = torch.cat(synth, dim=0).reshape_as(img)
-                error = F.mse_loss(img, synth)
-                psnr = -10*torch.log10(error)
-                view_psnrs.append(psnr)
-        
-        scene_psnr = torch.stack(view_psnrs).mean()
-        return scene_psnr
+    plt.show()
+    scene_psnr = torch.stack(view_psnrs).mean()
+    return scene_psnr
         
 
 def val_meta(args, model, val_loader, device):
@@ -198,9 +95,9 @@ def val_meta(args, model, val_loader, device):
         val_optim = torch.optim.SGD(val_model.parameters(), args.tto_lr)
 
         inner_loop(val_model, val_optim, tto_imgs, tto_poses, hwf,
-                    bound, args.num_samples, args.tto_batchsize, args.tto_steps, args.model)
+                    bound, args.num_samples, args.tto_batchsize, args.tto_steps)
         scene_psnr = report_result(val_model, test_imgs, test_poses, hwf, bound, 
-                                    args.num_samples, args.test_batchsize, args.tto_showImages, args.model)
+                                    args.num_samples, args.test_batchsize, args.tto_showImages)
         val_psnrs.append(scene_psnr)
 
     val_psnr = torch.stack(val_psnrs).mean()
@@ -213,8 +110,6 @@ def main():
                         help='config file for the shape class (cars, chairs or lamps)')
     parser.add_argument('--resume_step', type=int, default=0,
                         help='resume training from step')
-    parser.add_argument('--model', type=str, default='mip', choices=['simple', 'mip', 'ngp'],
-                        help='inner model, (simple, mip, ngp)')
     args = parser.parse_args()
 
     with open(args.config) as config:
@@ -238,11 +133,7 @@ def main():
         val_set = Subset(val_set, range(0, args.max_val_size))
     val_loader = DataLoader(val_set, batch_size=1, shuffle=False)
 
-    if args.model == 'mip':
-        meta_model = build_MipNerf(args)
-    elif args.model == 'simple':
-        meta_model = build_nerf(args)
-
+    meta_model = build_nerf(args)
     meta_model.to(device)
     
     if args.resume_step != 0:
@@ -271,7 +162,7 @@ def main():
     
             inner_loop(inner_model, inner_optim, imgs, poses,
                         hwf, bound, args.num_samples,
-                        args.train_batchsize, args.inner_steps, args.model)
+                        args.train_batchsize, args.inner_steps)
                         
             # Reptile
             with torch.no_grad():
